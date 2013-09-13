23cc38148902bf37a7275fb41bcba0d4ec0653a0
Laurent Orseau <laurent.orseau@gmail.com>
2013-09-13 22:35:16 +0200

    add file + add create-preferences

---
+++ towers-server/main.rkt
@@ -0,0 +1,21 @@
+#lang racket/base
+
+(require "server.rkt"
+         towers-lib/base
+         racket/cmdline)
+
+(parameterize ([current-logger towers-server-logger])
+  (command-line
+   #:once-each
+   [("-p" "--preferences") file
+                           "Sets the preference file"
+                           (pref-file file)]
+   [("--create-db") "Creates Towers database with empty tables if it does not exist"
+                    (create-towers-database)]
+   [("--create-prefs") "Ask for the default preferences and creates the preferences file"
+                       (create-preferences)]
+   [("--exit") "Does not run the server and exits immediately"
+                    (exit)]
+   #:args ()
+   (run-server)
+   ))
